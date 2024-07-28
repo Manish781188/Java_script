c2137859e0eea3b1a32323144def8d89dfdfc6d1
Manish yadav <manish781188@gmail.com>
2024-07-28 16:18:59 +0530

    java script

--- Asynchronous/promise.js
+++ Asynchronous/promise.js
@@ -1,53 +1,66 @@
 
 ///promise
 
-let promise =new Promise((res,rej)=>{
- let reason='Barish'
- if(reason==='barish'){
-    rej('F')
- }
- else{
-    res('T')
- }
-})
-console.log(promise);
-promise.then((a)=>{
-    console.log('resolve',a);
-}).catch((b)=>{
-    console.log('errer',b);
-})
+// let promise =new Promise((res,rej)=>{
+//  let reason='Barish'
+//  if(reason==='barish'){
+//     rej('F')
+//  }
+//  else{
+//     res('T')
+//  }
+// })
+// console.log(promise);
+// promise.then((a)=>{
+//     console.log('resolve',a);
+// }).catch((b)=>{
+//     console.log('errer',b);
+// })
 
-
-
-
-
-
-
-// ////call back hellllllllllllllllllllllll
-// function step1(fn){
-//     console.log(fn);
-//     setTimeout(()=>{
-//         console.log('select photo')
-//         fn()
-//     },5000)
+// /////////////////////
+let step1=  function(){
+   return new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res('select photo')
     
-// }
-// function step2(fn){
-    
-//     setTimeout(()=>{
-//         console.log('filteredd')
-//         fn()
-//     },4000)
-// }
-// function step3(fn){
-//     setTimeout(()=>{
-//         console.log('captioneddd')
-//         fn()
-//     },3000)
-// }
-// function step4(){
-//     setTimeout(()=>{
-//         console.log('posteddd')
-        
-//     },1000)
-// }
+    },5000)
+   })
+     
+}
+let step2=  function(){
+    return new Promise((res,rej)=>{
+     setTimeout(()=>{
+         res('filtereddd')
+     
+     },4000)
+    })
+}     
+
+ let step3=  function(){
+    return new Promise((res,rej)=>{
+     setTimeout(()=>{
+         res('captiosssss')
+     
+     },3000)
+    })   
+ }
+let step4=  function(){
+    return new Promise((res,rej)=>{
+     setTimeout(()=>{
+         res('postedd')
+     
+     },1000)
+    })    
+ }
+  step1().then((data)=>{
+    console.log(data);
+    return step2().then((filter)=>{
+       console.log(filter);
+       return step3().then((caption)=>{
+        console.log(caption);
+        return step4().then((post)=>{
+            console.log(post);
+        })
+       })
+    })
+})
